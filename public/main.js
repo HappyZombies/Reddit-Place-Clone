5c542d65f75122e0e6dfd09cf816901fdf348acc
Daniel <daniel.reguero@hotmail.com>
2017-04-04 18:35:44 -0400

    Ran checkstyle, fixed some comments, removed unused stuff.

--- public/main.js
+++ public/main.js
@@ -1,149 +1,138 @@
 // Global variables
-var selectedColor //store the selected color globally.
-var canvas //the <canvas>
-var context //the context of the canvas, aka what's inside it.
-var tempCanvas
-var tempContext
-var zoom = 20 //how big our pixel block is.
+var selectedColor // store the selected color globally.
+var canvas // the <canvas>
+var context // the context of the canvas, aka what's inside it.
+var pixelSize = 20 // how big our pixel block is.
 var screenX = $(window).width()
 var screenY = $(window).height()
 var isDown = false
 var startCoords = []
 var last = [0, 0]
-var windowWidth = 500;
-var windowHeight = 500
 var socket
-//^this is the actual size of the image. We want to zoom in on this and base it off that.
-//however when we save we need to assure that it's 500
 
-var windowX = 0;
-var windowY = 0;
+var windowX = 0
+var windowY = 0
 
-function initializeCanvas(){
-    canvas = $('#board').get(0) //get that canvas...
-    context = canvas.getContext("2d")
-    canvas.width = 0
-    canvas.height = 0
-	context.imageSmoothingEnabled = false;
-    context.mozImageSmoothingEnabled = false;
-    context.webkitImageSmoothingEnabled = false;
-    context.msImageSmoothingEnabled = false;
+function initializeCanvas () {
+  canvas = $('#board').get(0) // get that canvas...
+  context = canvas.getContext('2d')
+  canvas.width = 0
+  canvas.height = 0
+  context.imageSmoothingEnabled = false
+  context.mozImageSmoothingEnabled = false
+  context.webkitImageSmoothingEnabled = false
+  context.msImageSmoothingEnabled = false
 }
 
-function convertImageToCanvas(img) {
-	var tempCanvas = document.createElement("canvas");
-	tempCanvas.width = img.width;
-	tempCanvas.height = img.height;
-	tempCanvas.getContext("2d").drawImage(img, 0, 0);
-	return canvas;
+// Not used
+function convertImageToCanvas (img) {
+  var tempCanvas = document.createElement('canvas')
+  tempCanvas.width = img.width
+  tempCanvas.height = img.height
+  tempCanvas.getContext('2d').drawImage(img, 0, 0)
+  return canvas
 }
 
-function convertCanvasToImage(can) {
-	var tempImg = new Image();
-	tempImg.src = can.toDataURL("image/png");
-	return tempImg;
+function convertCanvasToImage (can) {
+  var tempImg = new Image()
+  tempImg.src = can.toDataURL('image/png')
+  return tempImg
 }
 
-function zoomIn(){
-    console.log("I was clicked")
-    imgContent.src = "./img/image.bmp"
-    imgContent.onload = function(){
-        canvas.width = imgContent.width
-        canvas.height = imgContent.height
-        context.drawImage(imgContent, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width*1.5, canvas.height*1.5)
-    }
+// Not used
+function zoomIn () {
+  console.log('I was clicked')
+  imgContent.src = './img/image.bmp'
+  imgContent.onload = function () {
+    canvas.width = imgContent.width
+    canvas.height = imgContent.height
+    context.drawImage(imgContent, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width * 1.5, canvas.height * 1.5)
+  }
 }
 
-function zoomOut(){
-    console.log("I was clicked")
-    imgContent.src = "./img/image.bmp"
-    imgContent.onload = function(){
-        canvas.width = imgContent.width
-        canvas.height = imgContent.height
-        context.drawImage(imgContent, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width*1, canvas.height*1)
-    }
+// Not used
+function zoomOut () {
+  console.log('I was clicked')
+  imgContent.src = './img/image.bmp'
+  imgContent.onload = function () {
+    canvas.width = imgContent.width
+    canvas.height = imgContent.height
+    context.drawImage(imgContent, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width * 1, canvas.height * 1)
+  }
 }
 
-//Start jQuery
-$(function(){
-    
-    //Declare our variables.
-    selectedColor = $("#selection")
-    initializeCanvas()
-    socket = io()
-    //draw the image.bmp to the canvas.
-    imgContent = new Image()
-    imgContent.src = "./img/image.bmp" //must be 500x500!
-    imgContent.onload = function(){
-        canvas.width = imgContent.width
-        canvas.height = imgContent.height
-    //     context.drawImage(photo, windowX, windowY, windowWidth, windowHeight, 0, 0,
-    // windowWidth,windowHeight);
-        context.drawImage(imgContent, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height)
+// Start jQuery
+$(function () {
+    // Declare our variables.
+  selectedColor = $('#selection')
+  initializeCanvas()
+  socket = io()
+    // When we begin, grab what is currently the image.bmp
+  imgContent = new Image()
+  imgContent.src = './img/image.bmp'
+  imgContent.onload = function () {
+    canvas.width = imgContent.width
+    canvas.height = imgContent.height
+    // By doing this, our  canvas will be whatever size is our image.bmp, right now it's 500x500
+    context.drawImage(imgContent, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height)
+  }
+  socket.on('load image', function (bin) {
+    imgContent.onload = function () {
+      canvas.width = imgContent.width
+      canvas.height = imgContent.height
+      context.drawImage(imgContent, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height)
     }
-     socket.on('load image', function(bin){
-        imgContent.onload = function(){
-            canvas.width = imgContent.width
-            canvas.height = imgContent.height
-            context.drawImage(imgContent, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height)
-        }
-        imgContent.src = bin;
-    })
+    imgContent.src = bin
+  })
 
-    canvas.onmousedown = function(e){
-        if(selectedColor.css('display') != 'none'){
-            var x = Math.floor(selectedColor.x)
-            var y = Math.floor(selectedColor.y)
-            isDown = true
-            startCoords = [x - last[0], y - last[0]]
-            context.fillStyle = selectedColor.css('background-color')
-            context.fillRect(x, y, zoom, zoom)
-            selectedColor.hide()
-            //user put in a block, run socket
-            socket.emit('load image', convertCanvasToImage(canvas).src)
-        }
+  canvas.onmousedown = function (e) {
+    if (selectedColor.css('display') != 'none') {
+      var x = Math.floor(selectedColor.x)
+      var y = Math.floor(selectedColor.y)
+      isDown = true
+      startCoords = [x - last[0], y - last[0]]
+      context.fillStyle = selectedColor.css('background-color')
+      context.fillRect(x, y, pixelSize, pixelSize)
+      selectedColor.hide()
+            // user put in a block, run socket
+      socket.emit('load image', convertCanvasToImage(canvas).src)
     }
+  }
 
-    canvas.onmouseup = function(e){
-        isDown = false
-        last = [
-            e.offsetX - startCoords[0],
-            e.offsetY - startCoords[1]
-        ]
+  canvas.onmouseup = function (e) {
+    isDown = false
+    last = [
+      e.offsetX - startCoords[0],
+      e.offsetY - startCoords[1]
+    ]
+  }
+
+  canvas.onmousemove = function (e) {
+    if (!isDown) return
+        // else pan the canvas...somehow.
+    console.log('user is holding mouse and moving on canvas...pan...')
+  }
+
+    // When a user clicks on a div inside the palettes, store the data of that div to our other div (selectedColor)
+  $('#palettes div').click(function (e) {
+    selectedColor.css('background-color', $(this).css('background-color'))
+    selectedColor.attr('class', $(this).attr('class'))
+    selectedColor.show() // by default we set display:none.
+  })
+
+    // this will always track the mouse movement.
+    // But we only want to follow/write to the variable if we are display: block for the selected color.
+  $(document).mousemove(function (e) {
+    if (selectedColor.css('display') !== 'none') {
+	       selectedColor.x = (Math.floor(e.pageX / pixelSize) * pixelSize)
+	       selectedColor.y = (Math.floor(e.pageY / pixelSize) * pixelSize)
+	       selectedColor.css('left', selectedColor.x + 1)
+	       selectedColor.css('top', selectedColor.y + 1)
+	       selectedColor.css('width', pixelSize)
+	       selectedColor.css('height', pixelSize)
     }
-
-    canvas.onmousemove = function(e){
-        if(!isDown) return;
-        //else pan the canvas...somehow.
-        console.log("user is holding mouse and moving on canvas...pan...")
-    }
-
-    //When a user clicks on a div inside the palettes, store the data of that div to our other div (selectedColor)
-    $('#palettes div').click(function(e){
-        selectedColor.css('background-color', $(this).css("background-color"))
-        selectedColor.attr('class', $(this).attr('class'))
-        selectedColor.show() //by default we set display:none.
-    })
-
-    //this will always track the mouse movement.
-    //But we only want to follow/write to the variable if we are display: block for the selected color.
-   $(document).mousemove(function(e){
-       if(selectedColor.css('display') != 'none'){
-	       selectedColor.x = (Math.floor(e.pageX / zoom) * zoom)
-	       selectedColor.y = (Math.floor(e.pageY / zoom) * zoom)
-	       selectedColor.css("left", selectedColor.x+1)
-	       selectedColor.css("top", selectedColor.y+1)
-	       selectedColor.css("width", zoom)
-	       selectedColor.css("height", zoom)
-           //attempt at something cool.
-            //    context.rect(selectedColor.x, selectedColor.y, zoom, zoom)
-            //    context.fillStyle = "rgba(0,0,0, 0.3)"
-            //    context.strokeStyle = 'black'
-            //    context.fill()
-            //    context.stroke()
-       }
-	})
-    $(window).resize(function(e){
-        //do something here to account for window resize. 
-    })
+  })
+  $(window).resize(function (e) {
+        // do something here to account for window resize.
+  })
 })
