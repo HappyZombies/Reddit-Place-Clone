6b1d53ea6c03b1e9757de55d0a72761955bd6fb7
Daniel <daniel.reguero@hotmail.com>
2017-04-07 08:48:12 -0400

    Hide config.js file with google id and stuff.

---
+++ config-example.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'googleAuth':{
+        'clientID': '',
+        'clientSecret': '',
+        'callbackURL': 'http://127.0.0.1:3000/auth/google/callback' //this can be different
+    }
+}
